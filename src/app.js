fbd5f7463010256cb15a2e2b2f197aba77652ea5
bingqichen <bingqichen@live.cn>
2017-04-06 09:16:26 +0800

    添加reudx-logger

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import dva from 'dva';
 import { browserHistory } from 'dva/router';
+import { createLogger } from 'redux-logger';
 
 import topicsListModel from './models/topicsList';
 import topicDetailModel from './models/topicDetail';
 import router from './router';
 
-const app = dva({
+const opts = {
   history: browserHistory,
   initialState: window.__INITIAL_STATE__
-});
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  opts.onAction = createLogger();
+}
+
+const app = dva(opts);
 
 app.model(topicsListModel);
 app.model(topicDetailModel);
